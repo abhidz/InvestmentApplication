7166ecbe31fcb42eabc48561b502a1fea7258993
Abhishek kumar <abhishek.kumar@avalara.com>
2022-01-15 08:36:09 +0530

    Include tearrform files

---
+++ Investment_App/AngularOKTA/Terraform/main.tf
@@ -0,0 +1,27 @@
+﻿terraform{
+    required_version = ">=0.12"
+
+    required_providers {
+      azurerm = {
+          source = "hashicorp/azurerm"
+      version = "~>2.0"
+      }
+    }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg-name" {
+    name = var.rgname
+    location = var.location
+}
+
+resource "azurerm_container_registry" "azacr" {
+    name = var.container_registry_name
+    resource_group_name = var.rgname
+    location = var.location
+    sku = "Standard"
+    admin_enabled = true
+}
---
+++ Investment_App/AngularOKTA/Terraform/variables.tf
@@ -0,0 +1,20 @@
+﻿variable "rgname" {
+  default   = "InvestmentApp_ResourceGroup"
+}
+
+variable "container_registry_name" {
+  default   = "InvestmentAppAzureContainerRegistry"
+}
+
+variable "location" {
+  default = "eastus"
+  description   = "Location of the resource group."
+}
+
+locals {
+ env_variables = {
+   DOCKER_REGISTRY_SERVER_URL            = "investmentappazurecontainerregistry.azurecr.io"
+   DOCKER_REGISTRY_SERVER_USERNAME       = "InvestmentAppAzureContainerRegistry"
+   DOCKER_REGISTRY_SERVER_PASSWORD       = "4ywhHQoYMQn5vNEj+uqEIijX1fdKvue2"
+ }
+}
